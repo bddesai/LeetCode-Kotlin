4ca3a2a8f4a5bf27819c1960dc21a586b45fb1c9
Bhavin <bhavin.desai@mattel.com>
2023-11-05 20:24:39 -0500

    ValidPalindromeII.kt - modified with optimal solution https://leetcode.com/problems/valid-palindrome-ii/

--- src/two_pointer/ValidPalindromeII.kt
+++ src/two_pointer/ValidPalindromeII.kt
@@ -2,43 +2,35 @@ package two_pointer
 
 // link: https://leetcode.com/problems/valid-palindrome-ii/
 
-class ValidPalindromeII {
-
+fun main() {
+    // Test cases
+    println(isPalindromeII("aba")) // Output: true
+    println(isPalindromeII("abca")) // Output: true
+    println(isPalindromeII("abc")) // Output: false
 }
 
 fun isPalindromeII(s: String): Boolean {
-    if(s.isEmpty()) return true
-
     var left = 0
     var right = s.length - 1
-
-    while(left<right){
-        if(!s[left].isLetterOrDigit()){
-            left++
-            continue
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return isPalindromeRange(s, left + 1, right) || isPalindromeRange(s, left, right - 1)
         }
-
-        if (!s[right].isLetterOrDigit()){
-            right--
-            continue
-        }
-
-        if(s[left].toLowerCase() != s[right].toLowerCase()){
-            return false
-        }
-
         left++
         right--
     }
-
     return true
 }
 
-fun isPalindrome(s: String, left: Int, right: Int): Boolean{
-    while(left < right){
-        if(s[left] != s[right]){
+fun isPalindromeRange(str: String, i: Int, j: Int): Boolean {
+    var start = i
+    var end = j
+    while (start < end) {
+        if (str[start] != str[end]) {
             return false
         }
+        start++
+        end--
     }
     return true
 }
