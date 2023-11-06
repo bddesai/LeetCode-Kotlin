78b7410ca609e3e3d687fe4af30183e90372eadc
Bhavin <bhavin.desai@mattel.com>
2023-11-05 21:41:00 -0500

    1. TwoSum.kt - Add Test Cases

--- src/two_pointer/TwoSum.kt
+++ src/two_pointer/TwoSum.kt
@@ -2,8 +2,31 @@ package two_pointer
 
 
 fun main() {
-    val result = twoSum(intArrayOf(2, 7, 11, 15), 9)
-    println(result?.joinToString())  // Output: 0, 1 (or similar, based on the input array's content)
+//    val result = twoSum(intArrayOf(2, 7, 11, 15), 9)
+//    println(result?.joinToString())  // Output: 0, 1 (or similar, based on the input array's content)
+
+    val testCases = arrayOf(
+        intArrayOf(2, 7, 11, 15) to 9,
+        intArrayOf(3, 2, 4) to 6,
+        intArrayOf(3, 3) to 6,
+        intArrayOf(-1, -2, -3, -4, -5) to -8,
+        intArrayOf(1, 5, 3, 7, 9) to 10,
+        intArrayOf(2, 1, 5, 3) to 4,
+        intArrayOf(0, 4, 3, 0) to 0,
+        intArrayOf(-3, 4, 5, 90) to 0
+    )
+
+    var count = 0
+    for ((nums, target) in testCases) {
+        count++
+        val result = twoSum(nums, target)
+        println("$count. Input: nums = ${nums.joinToString()}, target = $target")
+        if (result != null) {
+            println("> Output: ${result.joinToString()} ")
+        } else {
+            println("! Output: No two sum solution")
+        }
+    }
 }
 
 fun twoSum(nums: IntArray, target: Int): IntArray? {
