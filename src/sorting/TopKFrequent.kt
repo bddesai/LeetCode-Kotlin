8fd1a4baba8d0411621cee3b75299f5fde825c62
Bhavin <bhavin.desai@mattel.com>
2023-11-19 13:52:44 -0500

    top k frequent - new solution

--- src/sorting/TopKFrequent.kt
+++ src/sorting/TopKFrequent.kt
@@ -5,43 +5,64 @@ import java.util.*
 // link: https://leetcode.com/problems/top-k-frequent-elements/
 
 fun main() {
-    topKFrequent(intArrayOf(1,1,1,2,2,3), 2).forEach { print("$it ") }
-    topKFrequent(intArrayOf(1, 1, 1, 2, 2, 3), 2).contentToString()
+    topKFrequent2(intArrayOf(1, 1, 1, 2, 2, 3), 2).forEach { print("$it ") }
+    println("")
+    topKFrequent2(intArrayOf(1, 2, 3, 4, 4), 2).forEach { print("$it ") }
 }
 
 fun topKFrequent(nums: IntArray, k: Int): IntArray {
 
     // key - num, value - frequency
-    val map = mutableMapOf<Int, Int>()
+    val countMap = mutableMapOf<Int, Int>()
 
     // max heap - max frequency at the top and sorted by frequency in descending order
     val heap = PriorityQueue<IntArray>(compareBy { it[1] })
 
     for (i in nums.indices) {
-        map[nums[i]] = map.getOrDefault(nums[i], 0) + 1
+        countMap[nums[i]] = countMap.getOrDefault(nums[i], 0) + 1
     }
 
-    println(map)
+    println(countMap)
 
-    for (key in map.keys) {
+    for (key in countMap.keys) {
         val first = key
-        val second = map[key]
+        val second = countMap[key]
 
         heap.add(intArrayOf(first, second!!))
 
         if (heap.size > k) {
             val polledValue = heap.poll()
-            println("polledValue = ${polledValue.contentToString()}")
+            println("polledValue = ${polledValue!!.contentToString()}")
         }
     }
 
     //print("heap = $heap")
     val result = IntArray(k)
-    var i = k-1
+    var i = k - 1
     while (!heap.isEmpty()) {
         result[i] = heap.poll()[0]
         i--
     }
 
     return result
+}
+
+
+fun topKFrequent2(nums: IntArray, k: Int): IntArray {
+    val numsMap = hashMapOf<Int, Int>()
+    val priorityQueue = PriorityQueue<Pair<Int, Int>>(compareByDescending { it.second })
+
+    nums.forEach { num ->
+        numsMap[num] = numsMap.getOrDefault(num, 0) + 1
+    }
+
+    for ((key, value) in numsMap) {
+        priorityQueue.add(Pair(key, value))
+    }
+
+    val res = IntArray(k)
+    for (i in 0 until k) {
+        res[i] = priorityQueue.poll().first
+    }
+    return res
 }
