058ecccf65104f311ec5bb0f2481703a01aadb70
Bhavin <bhavin.desai@mattel.com>
2023-12-01 18:28:53 -0500

    11. Container With Most Water

--- src/two_pointer/ContainerWithMostWater.kt
+++ src/two_pointer/ContainerWithMostWater.kt
@@ -11,9 +11,24 @@ fun main() {
     )
 }
 
+// NeetCode => O(n)
 fun containerWithMostWater(nums: IntArray): Int {
-    var start = 0
-    var end = nums.size - 1
+    var (left, right) = 0 to nums.size - 1
+
+    var maxCapacity = 0
+    while (left < right) {
+        val area = (right-left) * minOf(nums[left], nums[right])
+        maxCapacity = maxOf(maxCapacity, area)
+
+        if(nums[left] < nums[right]) left++
+        else right--
+    }
+    return  maxCapacity
+}
+
+//  My Solution => O(n)
+fun containerWithMostWater2(nums: IntArray): Int {
+    var (start, end) = 0 to nums.size - 1
 
     var maxCapacity = 0
     while (start < end) {
