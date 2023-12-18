5d9b7783e8e1c7bdb5109a0c6d95137c2ef80211
Bhavin <bhavin.desai@mattel.com>
2023-12-18 18:24:44 -0500

    20. Valid Parentheses

--- src/stack/ValidParentheses.kt
+++ src/stack/ValidParentheses.kt
@@ -27,3 +27,18 @@ fun isValid(s: String): Boolean {
     return stack.isEmpty()
 }
 
+fun isValid2(s: String): Boolean {
+
+    val stack = Stack<Char>()
+    for (char in s) {
+        when (char) {
+            '(', '{', '[' -> stack.push(char)
+            ')' -> if (stack.isEmpty() || stack.pop() != '(') return false
+            '}' -> if (stack.isEmpty() || stack.pop() != '{') return false
+            ']' -> if (stack.isEmpty() || stack.pop() != '[') return false
+
+        }
+    }
+    return stack.isEmpty()
+}
+
